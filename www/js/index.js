8c20c051143112a70e8f4689d22f673f533c8f22
indahlissi <48428993+indahlissi@users.noreply.github.com>
2019-04-08 13:13:31 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=HB15HypZhDlWbz6tQMkmY905cXHhMJrK70ncYqJd";
         
         $("#loadImage").click(function(){
             $.ajax({
